e378ca465ff132974b70ff823d2427e91cdc94fe
Dillon Fagan <dillonfagan@pm.me>
2022-11-03 11:38:29 -0400

    Upgrade to Solidity v0.8.17

--- compile.js
+++ compile.js
@@ -5,7 +5,22 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-const result = solc.compile(source, 1);
-console.log(result);
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'],
+            },
+        },
+    },
+};
 
-module.exports = result.contracts[':Inbox'];
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+    'Inbox.sol'
+].Inbox;
--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
     '[seed phrase]',
@@ -13,11 +13,12 @@ const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hello, world!'] })
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hello, world!'] })
         .send({ gas: '1000000', from: accounts[0] });
     
     console.log('Contract deployed to', result.options.address);
+    provider.engine.stop()
 };
 
 deploy();
